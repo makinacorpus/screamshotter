aa92627b6bf6c2066454671b37646557272f661d
J-E Castagnede <j.e.castagnede@gmail.com>
2018-05-04 10:40:06 +0200

    update to django 1.11 fix

--- screamshotter/urls.py
+++ screamshotter/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 
 
 urlpatterns = [
-    url(r'^$',  include('screamshot.urls', namespace='screamshot', app_name='screamshot')),
+    url(r'',  include('screamshot.urls', namespace='screamshot', app_name='screamshot')),
 ]
