4c69f99db26fb80197d696de5a9daf1aebaddc33
J-E Castagnede <j.e.castagnede@gmail.com>
2021-04-19 12:28:41 +0200

    back to dev

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='screamshotter',
-    version='2.0.1',
+    version='2.0.1+dev',
     author='Makina Corpus',
     author_email='support.geotrek@makina-corpus.com',
     url='https://makina-corpus.com',
