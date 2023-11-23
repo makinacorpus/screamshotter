3d1787af90af1dc8542b29a693812b4481f88d4a
J-E Castagnede <j.e.castagnede@gmail.com>
2023-11-23 10:52:40 +0100

    fix subprocess remaining

--- src/screenshotter/puppeteer.py
+++ src/screenshotter/puppeteer.py
@@ -19,8 +19,7 @@ def take_screenshot(url, width=1920, height=1080, waitfor='body', wait_selectors
     # We send sentry informations and version : when we use screamshotter as a package, informations are in settings only
     with NamedTemporaryFile(suffix='.png') as screenshot_file:
         command = subprocess.run([
-            "dumb-init",
-            "--dumb-init",
+            "/usr/bin/dumb-init",
             "--",
             os.getenv('NODE_BIN_PATH', 'node'),
             app_settings.PUPPETEER_JAVASCRIPT_FILEPATH,
